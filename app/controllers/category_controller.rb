a1deff58c88260b2aeb0459614bd18fe29b1c200
Santiago Giraldo <santigibo@hotmail.com>
2020-02-25 11:54:48 -0500

    Created the controllers and the routes

---
+++ app/controllers/category_controller.rb
@@ -0,0 +1,9 @@
+class CategoryController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def show
+  end
+end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,3 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
-
-  def home
-  end
 end
