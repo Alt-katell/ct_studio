fa757d6cdf38d3fff6afbba2c808c5ba09fcaf12
Felix Timmel <f.timmel95@gmail.com>
2020-02-25 10:45:58 -0500

    get rid off the dependent destroy and debugg

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  has_many :design_categories, dependent: :destroy
+  has_many :design_categories
   has_many :designs, through: :design_categories
 
   validates :name, presence: true, inclusion: { in: ["Abstract", "Animal", "Colors", "Floral", "Geometry", "Illustration", "Kids", "Monochrome", "Placement"] }
