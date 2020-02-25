3ee80d52ded38b78d036d3b8da8fad077d0381c5
Santiago Giraldo <santigibo@hotmail.com>
2020-02-25 14:13:10 -0500

    Created the seeds file. Error destroying a category, TODO check error

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,26 +10,54 @@ require 'faker'
 
 cat = %W(Abstract Animal Colors Floral Geometry Illustration Kids Monochrome Placement)
 tags = %W(homedesign branding photooftheday like m instagram bhfyp designinspiration beautiful digitalart lifestyle vintage sketch graphic arte photoshop marketing architect modern arquitetura wood painting follow illustrator picoftheday beauty archilovers construction nature madeinitaly)
-url = "https://source.unsplash.com/collection/1352185/#{(1..1000).to_a.sample}"
 
-2.time do
-  User.create(first_name: "Sam", last_name: "El", address:"nfenoe", phone_number: "123", email:"sam@web.com", password:"123456")
+
+2.times do
+  n = Faker::Name.first_name
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone, email:"#{n.downcase}@web.com", password:"123456")
 end
 
 2.times do
-  User.create(first_name: "Sam", last_name: "El", address:"nfenoe", phone_number: "123", email:"sam@web.com", password:"123456", company: true, company_name: , website: )
+  n = Faker::Name.first_name
+  cn = Faker::Company.name
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone, email:"#{n.downcase}@web.com", password:"123456", company: true, company_name: cn, website: "www.#{cn}.com")
 end
 
+cat.each do |c|
+  Category.create(name: c)
+end
 
+tags.each do |t|
+  Tag.create(name: t)
+end
 
-file = URI.open('https://images.unsplash.com/photo-1565373987291-4d7424dd9e59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=526&q=80')
-d = Design.new({ name: 'poteto', file_type:"psd", pattern:"placement", price:1 })
-c = Category.create(name: 'Abstract')
-d.categories << c
-t = Tag.create(name: 'about')
-d.tags << t
-user1 = User.new({ first_name: "Sam", last_name: "El", address:"nfenoe", phone_number: "123", email:"sam@web.com", password:"123456" })
-user1.save
-d.user = user1
-d.photo.attach(io: file, filename: 'first.jpg', content_type: 'image/png')
-d.save
+20.times do
+  cats_d = Category.all.sample((1..3).to_a.sample)
+  tags_d = Tag.all.sample((1..5).to_a.sample)
+  user_d = User.where('company = ?', false).sample
+  n = Faker::Beer.brand
+  d = Design.new(name: n, file_type: %w(psd ai).sample, pattern: ['seamless tile', 'placement'].sample, price: (200..400).to_a.sample)
+  cats_d.each do |c|
+    d.categories << c
+  end
+  tags_d.each do |t|
+    d.tags << t
+  end
+  url = "https://source.unsplash.com/collection/1352185/#{(1..1000).to_a.sample}"
+  file = URI.open(url)
+  d.photo.attach(io: file, filename: "#{n}.jpg", content_type: 'image/png')
+  d.user = user_d
+  d.save
+end
+
+# file = URI.open('https://images.unsplash.com/photo-1565373987291-4d7424dd9e59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=526&q=80')
+# d = Design.new({ name: 'poteto', file_type:"psd", pattern:"placement", price:1 })
+# c = Category.create(name: 'Abstract')
+# d.categories << c
+# t = Tag.create(name: 'about')
+# d.tags << t
+# user1 = User.new({ first_name: "Sam", last_name: "El", address:"nfenoe", phone_number: "123", email:"sam@web.com", password:"123456" })
+# user1.save
+# d.user = user1
+# d.photo.attach(io: file, filename: 'first.jpg', content_type: 'image/png')
+# d.save
