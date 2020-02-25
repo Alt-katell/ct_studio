3a633d223ccbcbd83eaa84f172bda227a9e9bb33
Vicky Andre <v_key13@hotmail.com>
2020-02-25 14:00:43 -0500

    created DesignCOntroller

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -1,2 +1,46 @@
 class DesignsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @designs = Design.all
+  end
+
+  def show
+    @design = Design.find(params[:id])
+  end
+
+  def new
+    @design = Design.new
+  end
+
+  def create
+    @design = Design.new(design_params)
+    if @design.save
+      redirect_to design_path(@design)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @design = Design.find(params[:id])
+  end
+
+  def update
+    @design = Design.find(params[:id])
+    @design.update(design_params)
+  end
+
+  def destroy
+    @design = Design.find(params[:id])
+    @design.destroy
+  end
+
+  private
+
+  def design_params
+    params.require(:design).permit(:name, :tags, :file_type, :categories, :indexed_colors, :layers, :pattern, :price, :photo )
+  end
+
+
 end
