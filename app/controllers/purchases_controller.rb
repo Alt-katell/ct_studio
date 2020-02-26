f1567d24a1038fa4a87214938c3dfaac906b8574
Katell Goaer <alt.katellgoaer@gmail.com>
2020-02-26 14:00:28 -0500

    kept working on the purchase controller

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,8 +3,13 @@ class PurchasesController < ApplicationController
 
   def create
     @user = current_user
-    @design = Design.find(params[])
+    @design = Design.find(params[:design_id])
 
-    @purchase = Purchase.new()
+    @purchase = Purchase.create({ user_id: @user.id, design_id: @design.id })
+    redirect_to design_purchase_path(@purchase)
+  end
+
+  def show
+    @purchase = Purchase.find(params[:id])
   end
 end
