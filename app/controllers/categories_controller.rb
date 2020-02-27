4d3761a726d512da55fb52db61bcaee55f7ad47c
Vicky Andre <v_key13@hotmail.com>
2020-02-27 15:18:21 -0500

    corrected temporary breadcrumb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,8 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
-  add_breadcrumb "Home", :root_path
-  add_breadcrumb "Categories", :categories_path
-
-
   def index
     @categories = Category.all
-    add_breadcrumb "index", index_path
   end
 
   def show
