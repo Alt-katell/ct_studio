8b93e30c16e7337e65033989aea34ad5d8bb7dc5
Katell Goaer <alt.katellgoaer@gmail.com>
2020-02-28 15:41:57 -0500

    corrected mispelling mistakes

--- app/javascript/components/login.js
+++ app/javascript/components/login.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicLoginText = () => {
   if(document.querySelector('#login-typed-text')) {
     new Typed('#login-typed-text', {
-      strings: ["Explore our catalog", "and find the perfect print to your products"],
+      strings: ["Explore our catalog", "and find the perfect print for your products"],
       typeSpeed: 50,
       loop: true
     });
